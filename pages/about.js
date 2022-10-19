79bdedc95b051e6253a6371497e645ae5be19bee
Martin Tamareu <mg.tamareu@gmail.com>
2022-10-18 22:20:03 -0300

    Initial commit proj

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, ab?</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, ab?</p>
+        </div>
+    );
+}
+ 
+export default About;
