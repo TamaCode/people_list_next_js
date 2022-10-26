6ae94313342e02ab44b556a5409366c8c2f3af5a
Martin Tamareu <mg.tamareu@gmail.com>
2022-10-26 01:29:08 -0300

    Agrego estilos

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
     return ( 
-        <div>
+        <footer>
             <p>Copyright {new Date().getFullYear()} people list</p>
-        </div>
+        </footer>
      );
 }
  
