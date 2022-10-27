ba5882a6c55247bbb2ce1ed28217dddf46d3b470
Martin Tamareu <mg.tamareu@gmail.com>
2022-10-27 00:27:09 -0300

    Uso el getStaticProps para fetchear la data de cada person y luego utilizarla para renderizar el componente Detail de cada pagina

--- pages/people/[id].js
+++ pages/people/[id].js
@@ -1,3 +1,4 @@
+// Con este metodo Next puede saber con anticipacion cuantos html files (de ruta dinamica) debe pre renderizar segun el length del array paths
 export const getStaticPaths = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await res.json();
@@ -13,10 +14,26 @@ export const getStaticPaths = async () => {
     }
 };
 
-const Details = () => {
+// Esta funcion corre tantas veces como sea el length del array paths. Es decir una vez por person ID.
+// La idea con esto es que se puedan pre renderizar 10 html files (en este caso) en donde va a haber un componente Detail en cada uno haciendo referencia a un id
+
+export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+    const data = await res.json();
+
+    return {
+        props: { person: data }
+    }
+};
+
+const Details = ({ person }) => {
     return (
         <div>
-            <h1>Details Page</h1>
+            <h1>{person.name}</h1>
+            <p>{person.email}</p>
+            <p>{person.website}</p>
+            <p>{person.city}</p>
         </div>
     );
 }
