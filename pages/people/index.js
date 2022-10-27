be19a6671705a17e14c547ba337acb6a4c6ac727
Martin Tamareu <mg.tamareu@gmail.com>
2022-10-26 22:47:46 -0300

    fetcheo data con getStaticProps y creo componente People

--- pages/people/index.js
+++ pages/people/index.js
@@ -1,7 +1,33 @@
-const People = () => {
+import styles from '../../styles/People.module.css'
+
+// Este metodo corre antes de que el componente People se renderice. (Propio de NEXT)
+// No usar codigo que tendria que correr en el navegador
+// Recordar que next pre-renderiza los componentes del lado del servidor
+// La carga inicial de datos dentro de un componente se deberia hacer dentro de getStaticProps y no dentro de useEffect porque no estariamos aprovechando la ventaja que nos da Next de pre-renderizar del lado del server y hacer mas rapida nuestra app.
+
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return  {
+        // props es accesible desde People como parametro
+        props: {
+            people: data
+        }
+    }
+};
+
+const People = ({ people }) => {
     return (
         <div>
             <h1>People</h1>
+            {people.map(person => (
+                <div key={person.id}>
+                    <a className={styles.single}>
+                        <h3>{person.name}</h3>
+                    </a>
+                </div>
+            ))}
         </div>
     );
 }
